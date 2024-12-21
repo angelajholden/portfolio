7c67e6b7e3b67a72f746a35c7e213f8895a997fd
Angela J Holden <angelajholden@gmail.com>
2024-12-21 15:38:56 -0600

    form styles

--- contact/index.php
+++ contact/index.php
@@ -18,19 +18,26 @@ include_once dirname(__DIR__) . '/config.php';
 	<?php include_once dirname(__DIR__) . '/includes/header.php'; ?>
 	<main class="main">
 		<section class="contact">
-			<!-- modify this form HTML and place wherever you want your form -->
-			<form action="https://formspree.io/f/xyzyqyve" method="POST">
-				<label>
-					Your email:
-					<input type="email" name="email">
-				</label>
-				<label>
-					Your message:
-					<textarea name="message"></textarea>
-				</label>
-				<!-- your other form fields go here -->
-				<button type="submit">Send</button>
-			</form>
+			<div class="wrap">
+				<h2 class="secondary-heading">Contact Me</h2>
+				<!-- modify this form HTML and place wherever you want your form -->
+				<form class="form" action="https://formspree.io/f/xyzyqyve" method="POST">
+					<div class="form-inputs">
+						<label class="access-hidden" for="first_name">First Name (required)</label>
+						<input id="first_name" name="first_name" type="text" placeholder="First Name (required)" required="required">
+						<label class="access-hidden" for="last_name">Last Name (required)</label>
+						<input id="last_name" name="last_name" type="text" placeholder="Last Name (required)" required="required">
+						<label class="access-hidden" for="email">Email (required)</label>
+						<input id="email" type="email" name="email" placeholder="Email (required)" required="required">
+						<label class="access-hidden" for="phone">Phone (optional)</label>
+						<input id="phone" name="phone" placeholder="Phone (optional)" type="tel">
+					</div>
+					<label class="access-hidden" for="message">Message (required)</label>
+					<textarea id="message" name="message" placeholder="Message (required)" rows="7" required="required"></textarea>
+					<!-- your other form fields go here -->
+					<button class="button teal-solid" type="submit">Submit</button>
+				</form>
+			</div>
 		</section>
 		<?php include_once dirname(__DIR__) . '/includes/social.php'; ?>
 	</main>
