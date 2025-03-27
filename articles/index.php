4a25112aca0f1083cac562f58d67a8b6e1ded8e5
Angela J Holden <angelajholden@gmail.com>
2025-03-27 10:05:37 -0500

    initial masonry layout for articles

--- articles/index.php
+++ articles/index.php
@@ -9,10 +9,95 @@ include_once dirname(__DIR__) . '/includes/head.php' ?>
 <body>
 	<?php include_once dirname(__DIR__) . '/includes/header.php'; ?>
 	<main id="content" class="main">
-		<section class="contact">
-			<div class="wrap">
-				<h2 class="secondary-heading">Articles</h2>
-				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum fugit qui modi accusantium animi voluptatibus numquam molestias quod minus unde, nisi quisquam id, sapiente eaque optio eum placeat perspiciatis harum sint ad nemo? Amet tempore expedita laudantium ut voluptas vero odio laborum sit aperiam facere suscipit quae omnis repellendus ratione ea quisquam reiciendis cumque voluptates a non aspernatur, dolorem quaerat. Esse veniam iure reprehenderit qui voluptatum quasi aspernatur necessitatibus, dolorum facere id sint pariatur hic. Molestias dicta dolor quod dolore doloremque ullam, iure totam hic unde consectetur veniam aperiam iste ex asperiores laudantium error reprehenderit temporibus quas architecto. Fugit.</p>
+		<section class="masonry-articles">
+			<header class="content-header">
+				<h1 class="primary-heading">Articles</h1>
+			</header>
+			<div class="masonry-wrap">
+				<article class="article-item">
+					<figure class="image">
+						<img src="<?php echo BASE_URL; ?>images/alexander-schimmeck-2zJhA9RSkys-unsplash.jpg" alt="Crates of fruit at an open market">
+					</figure>
+					<div class="article-wrap">
+						<div class="article-category">Tutorials</div>
+						<h2 class="secondary-heading">
+							<a href="#">Title One: Crates of fruit at an open market</a>
+						</h2>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ut saepe beatae placeat consectetur provident ipsa laboriosam quae exercitationem? Odio libero magnam voluptates corrupti distinctio sed, numquam dignissimos pariatur reiciendis.</p>
+					</div>
+				</article>
+				<article class="article-item">
+					<figure class="image">
+						<img src="<?php echo BASE_URL; ?>images/aliona-gumeniuk-jeAjT87nbjM-unsplash.jpg" alt="Cheese and red pomegranate seeds">
+					</figure>
+					<div class="article-wrap">
+						<div class="article-category">Tutorials</div>
+						<h2 class="secondary-heading">
+							<a href="#">Title Two: Cheese and red pomegranate seeds</a>
+						</h2>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ut saepe beatae placeat consectetur provident ipsa laboriosam quae exercitationem? Odio libero magnam voluptates corrupti distinctio sed, numquam dignissimos pariatur reiciendis.</p>
+					</div>
+				</article>
+				<article class="article-item">
+					<figure class="image">
+						<img src="<?php echo BASE_URL; ?>images/anna-bratiychuk-IeNoBmJ011g-unsplash.jpg" alt="Rosé wine in glasses">
+					</figure>
+					<div class="article-wrap">
+						<div class="article-category">Tutorials</div>
+						<h2 class="secondary-heading">
+							<a href="#">Title Three: Rosé wine in glasses</a>
+						</h2>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ut saepe beatae placeat consectetur provident ipsa laboriosam quae exercitationem? Odio libero magnam voluptates corrupti distinctio sed, numquam dignissimos pariatur reiciendis.</p>
+					</div>
+				</article>
+				<article class="article-item">
+					<figure class="image">
+						<img src="<?php echo BASE_URL; ?>images/annie-spratt-e92dhXE8PUg-unsplash.jpg" alt="Soft yellow tulips">
+					</figure>
+					<div class="article-wrap">
+						<div class="article-category">Tutorials</div>
+						<h2 class="secondary-heading">
+							<a href="#">Title Four: Soft yellow tulips</a>
+						</h2>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ut saepe beatae placeat consectetur provident ipsa laboriosam quae exercitationem? Odio libero magnam voluptates corrupti distinctio sed, numquam dignissimos pariatur reiciendis.</p>
+					</div>
+				</article>
+				<article class="article-item">
+					<figure class="image">
+						<img src="<?php echo BASE_URL; ?>images/annie-spratt-eZk9w9RBHRo-unsplash.jpg" alt="A single stem of rich, green eucalyptus">
+					</figure>
+					<div class="article-wrap">
+						<div class="article-category">Tutorials</div>
+						<h2 class="secondary-heading">
+							<a href="#">Title Five: A single stem of rich, green eucalyptus</a>
+						</h2>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ut saepe beatae placeat consectetur provident ipsa laboriosam quae exercitationem? Odio libero magnam voluptates corrupti distinctio sed, numquam dignissimos pariatur reiciendis.</p>
+					</div>
+				</article>
+				<article class="article-item">
+					<figure class="image">
+						<img src="<?php echo BASE_URL; ?>images/boys-in-bristol-photography-uOeXhx5JV04-unsplash.jpg" alt="Fuzzy bumblebee sitting on a purple flower bulb">
+					</figure>
+					<div class="article-wrap">
+						<div class="article-category">Tutorials</div>
+						<h2 class="secondary-heading">
+							<a href="#">Title Six: Fuzzy bumblebee sitting on a purple flower bulb</a>
+						</h2>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ut saepe beatae placeat consectetur provident ipsa laboriosam quae exercitationem? Odio libero magnam voluptates corrupti distinctio sed, numquam dignissimos pariatur reiciendis.</p>
+					</div>
+				</article>
+				<article class="article-item">
+					<figure class="image">
+						<img src="<?php echo BASE_URL; ?>images/brooke-lark-qdyBKWSzpSI-unsplash.jpg" alt="Fruit smoothie with mint and ginger">
+					</figure>
+					<div class="article-wrap">
+						<div class="article-category">Tutorials</div>
+						<h2 class="secondary-heading">
+							<a href="#">Title Seven: Fruit smoothie with mint and ginger</a>
+						</h2>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ut saepe beatae placeat consectetur provident ipsa laboriosam quae exercitationem? Odio libero magnam voluptates corrupti distinctio sed, numquam dignissimos pariatur reiciendis.</p>
+					</div>
+				</article>
 			</div>
 		</section>
 	</main>
