aba2991dde2b62fff69f4440d9762bcea6f98afd
Angela J Holden <angelajholden@gmail.com>
2025-03-27 14:04:58 -0500

    some more adjustments to mobile spacing

--- articles/index.php
+++ articles/index.php
@@ -19,7 +19,7 @@ include_once dirname(__DIR__) . '/includes/head.php' ?>
 						<img src="<?php echo BASE_URL; ?>images/alexander-schimmeck-2zJhA9RSkys-unsplash.jpg" alt="Crates of fruit at an open market">
 					</figure>
 					<div class="article-wrap">
-						<div class="article-category">Tutorials</div>
+						<div class="article-category">Tutorial</div>
 						<h2 class="secondary-heading"><a href="#">Title One: Crates of fruit at an open market</a></h2>
 						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ut saepe beatae placeat consectetur provident ipsa laboriosam quae exercitationem?</p>
 					</div>
@@ -29,7 +29,7 @@ include_once dirname(__DIR__) . '/includes/head.php' ?>
 						<img src="<?php echo BASE_URL; ?>images/aliona-gumeniuk-jeAjT87nbjM-unsplash.jpg" alt="Cheese and red pomegranate seeds">
 					</figure>
 					<div class="article-wrap">
-						<div class="article-category">Tutorials</div>
+						<div class="article-category">Tutorial</div>
 						<h2 class="secondary-heading"><a href="#">Title Two: Cheese and red pomegranate seeds</a></h2>
 						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ut saepe beatae placeat consectetur provident ipsa laboriosam quae exercitationem?</p>
 					</div>
@@ -39,7 +39,7 @@ include_once dirname(__DIR__) . '/includes/head.php' ?>
 						<img src="<?php echo BASE_URL; ?>images/anna-bratiychuk-IeNoBmJ011g-unsplash.jpg" alt="Rosé wine in glasses">
 					</figure>
 					<div class="article-wrap">
-						<div class="article-category">Tutorials</div>
+						<div class="article-category">Tutorial</div>
 						<h2 class="secondary-heading"><a href="#">Title Three: Rosé wine in glasses</a></h2>
 						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ut saepe beatae placeat consectetur provident ipsa laboriosam quae exercitationem?</p>
 					</div>
@@ -49,7 +49,7 @@ include_once dirname(__DIR__) . '/includes/head.php' ?>
 						<img src="<?php echo BASE_URL; ?>images/annie-spratt-e92dhXE8PUg-unsplash.jpg" alt="Soft yellow tulips">
 					</figure>
 					<div class="article-wrap">
-						<div class="article-category">Tutorials</div>
+						<div class="article-category">Tutorial</div>
 						<h2 class="secondary-heading"><a href="#">Title Four: Soft yellow tulips</a></h2>
 						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ut saepe beatae placeat consectetur provident ipsa laboriosam quae exercitationem?</p>
 					</div>
@@ -59,7 +59,7 @@ include_once dirname(__DIR__) . '/includes/head.php' ?>
 						<img src="<?php echo BASE_URL; ?>images/annie-spratt-eZk9w9RBHRo-unsplash.jpg" alt="A single stem of rich, green eucalyptus">
 					</figure>
 					<div class="article-wrap">
-						<div class="article-category">Tutorials</div>
+						<div class="article-category">Tutorial</div>
 						<h2 class="secondary-heading"><a href="#">Title Five: A single stem of rich, green eucalyptus</a></h2>
 						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ut saepe beatae placeat consectetur provident ipsa laboriosam quae exercitationem?</p>
 					</div>
@@ -69,7 +69,7 @@ include_once dirname(__DIR__) . '/includes/head.php' ?>
 						<img src="<?php echo BASE_URL; ?>images/boys-in-bristol-photography-uOeXhx5JV04-unsplash.jpg" alt="Fuzzy bumblebee sitting on a purple flower bulb">
 					</figure>
 					<div class="article-wrap">
-						<div class="article-category">Tutorials</div>
+						<div class="article-category">Tutorial</div>
 						<h2 class="secondary-heading"><a href="#">Title Six: Fuzzy bumblebee sitting on a purple flower bulb</a></h2>
 						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ut saepe beatae placeat consectetur provident ipsa laboriosam quae exercitationem?</p>
 					</div>
@@ -79,7 +79,7 @@ include_once dirname(__DIR__) . '/includes/head.php' ?>
 						<img src="<?php echo BASE_URL; ?>images/brooke-lark-qdyBKWSzpSI-unsplash.jpg" alt="Fruit smoothie with mint and ginger">
 					</figure>
 					<div class="article-wrap">
-						<div class="article-category">Tutorials</div>
+						<div class="article-category">Tutorial</div>
 						<h2 class="secondary-heading"><a href="#">Title Seven: Fruit smoothie with mint and ginger</a></h2>
 						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ut saepe beatae placeat consectetur provident ipsa laboriosam quae exercitationem?</p>
 					</div>
