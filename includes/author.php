4e98250f7468181d438544ae4d33ba4533b78ecc
Angela J Holden <angelajholden@gmail.com>
2025-03-30 20:07:26 -0500

    author, comments, forms, contact form

--- includes/author.php
+++ includes/author.php
@@ -1,4 +1,4 @@
-<footer class="author-box">
+<div class="author-box">
 	<h3 class="tertiary-header">About the Author</h3>
 	<div class="svg-border"></div>
 	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><path class="prefix__st0" d="M169.3 32c-4.1-6.3-9.8-8.9-15.9-9.2-1.4-.6-3.1-.8-4.8-.4-1.5.4-3 .9-4.5 1.5-9.4 2.5-19 8.5-25.6 13.5-7.1 5.3-13.3 11.7-18.5 18.7-3-9.2-7.5-17.9-13.5-25.7C76.8 17.9 59.7 1.5 41.7 5.6c-9.6 2.1-13.8 9.9-15 18.7-1.5 10.9-1.9 21.8-1.2 32.7 1.3 22 6.6 43.5 13.8 64.4 8.3 23.9 19 47 29.8 69.9 3.3 6.9 12.6 3 13.2-3.3 1.7.4 3.6.2 5-.8 33.1-24.8 59.5-57 75.9-94.3C171 75 181.4 50.4 169.3 32zm-50.9 38.5c5-6.9 10.3-13.7 15.9-20.2 1.1-1.3 2.2-2.5 3.3-3.8-9.6 24-25.4 45.4-37.5 68.5 0-2.5-.1-5-.2-7.5 0-2-.8-3.6-1.9-4.7 6.1-11.2 12.9-22 20.4-32.3zm-60.8-31l.2.2c1.2 1.6 2.4 3.3 3.5 5 .7 1 1.3 2 2 3-.9 1.1-1.4 2.5-1.3 4.3.4 7.2.8 14.4 1.2 21.7-.2-.8-.3-1.5-.5-2.3-2-9.3-4.1-18.8-5-28.3-.1-1.1-.1-2.3-.1-3.6zM101 156.9s.1.1 0 0v0zm8.9-25.3c7-11.6 14.7-22.7 23.1-33.3-6.9 11.4-14.9 22.5-23.1 33.3zm50.3-78.5c-2.2-.5-4.7 0-6.6 1.7-2.5 2.3-4.8 4.6-7.2 7 3.8-7.7 7-15.5 9.4-23.8 1.1.9 1.9 2.3 2.6 3.9 1.5 3.6 1.9 7.3 1.8 11.2zm-74.7 5.2C82 50.9 78 43.7 73.3 36.8c5.3 6.4 9.4 13.7 12.2 21.5zM41 26.3c.4-2.8.7-6.2 3.9-7.1 2.5-.7 5.8.2 8.3 1.2-2.2.2-4.5 1.2-6.6 3.5-6.6 6.8-3.5 19.6-2.3 27.8 3.4 23.4 9.4 46.6 14.3 69.8 1.3 6.5 2.4 13 3.6 19.5-8.1-19.7-15-39.8-19.1-60.7-3.5-17.5-4.8-36.2-2.1-54z"/></svg>
@@ -6,5 +6,5 @@
 		<img src="<?php echo BASE_URL ?>images/ajh-selfie.jpg" alt="Author, Angela J Holden">
 	</figure>
 	<h4 class="author-name">Angela J Holden</h4>
-	<p>After more than a decade as a frontend developer, I've worked on thousands of projects—ranging from small business sites to large-scale university schools and colleges—with experience in both building from scratch, and optimizing legacy codebases.</p>
-</footer>
+	<p>After more than a decade as a frontend developer, I've worked on thousands of projects—ranging from small business sites to large-scale university schools and colleges—with experience in both building from scratch, and optimizing legacy code.</p>
+</div>
---
+++ includes/comment-form.php
@@ -0,0 +1,19 @@
+<div class="comment-form">
+	<h3 class="tertiary-header">Leave a Comment</h3>
+	<div class="svg-border"></div>
+	<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M464 80H48a16 16 0 00-16 16v320a16 16 0 0016 16h416a16 16 0 0016-16V96a16 16 0 00-16-16zM265.82 284.63a16 16 0 01-19.64 0L89.55 162.81l19.64-25.26L256 251.73l146.81-114.18 19.64 25.26z"/></svg>
+	<form class="form" action="">
+		<div class="form-inputs">
+			<label class="access-hidden" for="name">Name</label>
+			<input id="name" name="name" type="text" placeholder="Name*" required="required" autocomplete="autocomplete">
+			<label class="access-hidden" for="email">Email</label>
+			<input id="email" type="email" name="email" placeholder="Email*" required="required" autocomplete="autocomplete">
+			<label class="access-hidden" for="website">Website</label>
+			<input id="website" name="website" placeholder="Website" type="url" autocomplete="autocomplete">
+		</div>
+		<label class="access-hidden" for="message">Message</label>
+		<textarea id="message" name="message" placeholder="Message*" rows="7" required="required"></textarea>
+		<!-- your other form fields go here -->
+		<button class="button teal-solid" type="submit">Submit Comment</button>
+	</form>
+</div>
