0221186dca6f0b5393b7c2f8669c9471fa50e849
Angela J Holden <angelajholden@gmail.com>
2025-06-15 09:14:56 -0500

    link checker for noopener, adding three content articles

---
+++ articles/how-to-practice-javascript/index.php
@@ -0,0 +1,145 @@
+<?php include_once dirname(__DIR__) . '/../config.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<?php 
+$title = 'How to Practice JavaScript | Angela J Holden | Software Engineer &amp; Frontend Developer';
+include_once dirname(__DIR__) . '/../includes/head.php' ?>
+
+<body>
+	<?php include_once dirname(__DIR__) . '/../includes/header.php'; ?>
+	<main id="content" class="main">
+		<div class="full-width_hero"></div>
+		<article class="full-width_content animate__animated animate__fadeIn">
+			<div class="wrap">
+				<header class="content-header">
+					<h1 class="primary-heading">How to Practice JavaScript, with an Example</h1>
+					<div class="content-meta">
+						<div class="meta-item by-author">
+							<figure class="meta-image">
+								<img src="<?php echo BASE_URL; ?>images/angelajholden-software-engineer-frontend-developer.jpg" alt="Angela J Holden | Software Engineer &amp; Frontend Developer">
+							</figure>
+							<span>By Angela</span>
+						</div>
+						<div class="meta-dots"></div>
+						<div class="meta-group">
+							<div class="meta-item">
+								<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M256 48C141.13 48 48 141.13 48 256s93.13 208 208 208 208-93.13 208-208S370.87 48 256 48zm96 240h-96a16 16 0 01-16-16V128a16 16 0 0132 0v128h80a16 16 0 010 32z"/></svg>
+								<time datetime="2025-06-15">Jun 15, 2025</time>
+							</div>
+							<div class="meta-item">
+								<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M400 480a16 16 0 01-10.63-4L256 357.41 122.63 476A16 16 0 0196 464V96a64.07 64.07 0 0164-64h192a64.07 64.07 0 0164 64v368a16 16 0 01-16 16z"/></svg>
+								<span>Tips &amp; Advice</span>
+							</div>
+						</div>
+					</div>
+				</header>
+				<div class="content-item">
+					<p>I love when I get questions from my subscribers, and I got this comment on a <a href="https://www.youtube.com/watch?v=AZIpWfafAEk" target="_blank">recent video</a>. It's the kind of question that deserves a deeper answer, so I wanted to share my thoughts here. Let's walk through it together, because if one person asked, chances are others are wondering the same thing.</p>
+
+					<blockquote>
+						Could you make a video with tips on learning JavaScript or getting better at it? I'm familiar with the basics and concepts, but I find JavaScript more challenging compared to HTML and CSS. (You and me both!) For example: writing code without looking at an example. Also, I'd love some advice on how to work with JavaScript—like what you do when you can't remember how to do something (which is normal, since using Google is part of the job). Where do you like to look for solutions? I personally use resources like MDN and W3Schools.
+					</blockquote>
+
+					<p>Before I dive in, I won't bury the lede. The answer is practice and repetition. JavaScript is a language, and to learn a new language you need to use it.</p>
+
+					<p>But let's break down this question a little more. First, I also find JavaScript a lot more challenging than HTML and CSS. I mean, I use JavaScript most days and it's probably always going to be the thing I need the most help with. You are not alone there at all.</p>
+
+					<p>Next, I don't think you should worry about needing to look at examples, although I totally understand the frustration of not remembering something. This is why I take really good notes and I version control most things I do. So I don't have to remember everything. Because even when I do something so well I can do it from memory, if I don't do it again for six months or a year, I sometimes forget and have to look it up.</p>
+
+					<p>I think MDN and W3Schools are both excellent resources. I also recommend Learn JavaScript which I'll link below. This is one of the best JavaScript tutorials I've ever worked on and it's structured in a way that makes you solve real problems over and over again.</p>
+
+					<p>Again, the best thing you can do to build your skills is practice and repetition. Think of all the user interface elements that require JavaScript. Things like tabs, accordions, sliders, navigation… those are all things that are done with JavaScript. I promise that if you practice building these things, you'll get much more comfortable and confident with JavaScript.</p>
+
+					<p>You know what else you can do? <a href="https://www.youtube.com/@angelajholden" target="_blank">Subscribe to my channel</a> and hit the Like button so I can keep answering your questions, and help you build confidence as developers. It helps me out and I really appreciate your support. Thanks!</p>
+
+					<p>So let me show you how I use a JavaScript <code class="language-javascript">.forEach();</code> loop and the <code class="language-javascript">window.location;</code> property to create something called You Are Here navigation. For-loops and JavaScript API properties are two very common, and very powerful, things you can use to manipulate the DOM.</p>
+
+					<p>Let's say we have this navigation of five links for five pages:</p>
+
+					<pre>
+						<code class="language-html">
+							&lt;!--typical navigation--&gt;
+							&lt;nav&gt;
+							&nbsp;&nbsp;&lt;ul&gt;
+							&nbsp;&nbsp;&nbsp;&nbsp;&lt;li&gt;&lt;a class=&quot;active&quot; href=&quot;&quot;&gt;Home&lt;/a&gt;&lt;/li&gt;
+							&nbsp;&nbsp;&nbsp;&nbsp;&lt;li&gt;&lt;a href=&quot;&quot;&gt;About&lt;/a&gt;&lt;/li&gt;
+							&nbsp;&nbsp;&nbsp;&nbsp;&lt;li&gt;&lt;a href=&quot;&quot;&gt;Portfolio&lt;/a&gt;&lt;/li&gt;
+							&nbsp;&nbsp;&nbsp;&nbsp;&lt;li&gt;&lt;a href=&quot;&quot;&gt;Blog&lt;/a&gt;&lt;/li&gt;
+							&nbsp;&nbsp;&nbsp;&nbsp;&lt;li&gt;&lt;a href=&quot;&quot;&gt;Contact&lt;/a&gt;&lt;/li&gt;
+							&nbsp;&nbsp;&lt;/ul&gt;
+							&lt;/nav&gt;
+						</code>
+					</pre>
+
+					<p>When I hover over the links, they're styled with CSS to have an underline. I also want an “active” class added to the link that belongs to the page I'm viewing, so if I'm on the home page, the home page link also has an active class that keeps the underline on the link.</p>
+
+					<p>Before I start, I'm going to write something called pseudo code: For each navigation link, if the link is the same link in the browser, add the active class to the link.</p>
+
+					<p>First, we need all of those links in an array:</p>
+
+					<pre>
+						<code class="language-javascript">
+							/*
+							* finds everything with this class and builds a static array
+							* https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
+							*/
+							const links = document.querySelectorAll('.nav-link');
+						</code>
+					</pre>
+
+					<p><code class="language-javascript">document.querySelectorAll();</code> takes everything with the class <code class="language-css">.nav-link {}</code> and creates an array, which we need in order to use a for loop. Next we need to get the link of the page we are currently on. So in the console if I type <code class="language-javascript">window.location;</code>, I get all of this information from the JavaScript API about the page I'm on. I want everything that comes after localhost, so I'm going to use <code class="language-javascript">window.location.href;</code>.</p>
+
+					<pre>
+						<code class="language-javascript">
+							/*
+							* compare the nav link to the URL
+							* if they match, add an 'active' class
+							*/
+							const links = document.querySelectorAll(".nav_item");
+							const href = window.location.href;
+							links.forEach((link) => {
+							&nbsp;&nbsp;if (link == href) {
+							&nbsp;&nbsp;&nbsp;&nbsp;link.classList.add('active');
+							&nbsp;&nbsp;}
+							});
+						</code>
+					</pre>
+
+					<p>So let's make a list of everything we did here:</p>
+
+					<ul>
+						<li>created an array of links</li>
+						<li>used the JavaScript API to get the link of the current page</li>
+						<li>used a for loop to check all the links and compare them to the link of the current page</li>
+						<li>added a class to the link that matches the page link</li>
+					</ul>
+
+					<p>Creating an array, using the JavaScript API, looping through an array, using a conditional <code>if</code> statement to see if something matches, then adding a class to the correct element… This is a basic example, but also a really common application of JavaScript. And when you practice regularly and apply even basic techniques, you'll start to remember what you need to do.</p>
+
+					<p>Keep writing pseudo code. Write what you need to do with JavaScript in plain language. If you can describe the problem you're solving, you can find the JavaScript you need to solve that problem.</p>
+
+					<p>This is why I don't think you should worry too much about remembering syntax. Those things come with experience, and if you're using something like VS Code, intellisense is pretty good at helping with that. What's most important is that you understand the problem you're solving and come up with the best approach to solve that problem.</p>
+
+					<h2>Links:</h2>
+					<ul>
+						<li><a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/API/Window/location">Window: location property</a></li>
+						<li><a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll">Document: querySelectorAll() method</a></li>
+						<li><a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach">Array.prototype.forEach()</a></li>
+						<li><a target="_blank" href="https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting">Dynamic scripting with JavaScript</a></li>
+						<li><a target="_blank" href="https://www.w3schools.com/js/">W3 Schools JavaScript Tutorial</a></li>
+						<li><a target="_blank" href="https://learnjavascript.online/">Learn JavaScript Online</a></li>
+					</ul>
+
+				</div>
+				<footer class="content-footer">
+					<?php include_once dirname(__DIR__) . '/../includes/author.php'; ?>
+					<?php include_once dirname(__DIR__) . '/../includes/comment-form.php'; ?>
+				</footer>
+			</div>
+		</article>
+	</main>
+	<?php include_once dirname(__DIR__) . '/../includes/footer.php'; ?>
+</body>
+
+</html>
---
+++ articles/how-to-start-freelancing/index.php
@@ -0,0 +1,160 @@
+<?php include_once dirname(__DIR__) . '/../config.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<?php 
+$title = 'How to Start Freelancing as a Frontend Developer | Angela J Holden | Software Engineer &amp; Frontend Developer';
+include_once dirname(__DIR__) . '/../includes/head.php' ?>
+
+<body>
+	<?php include_once dirname(__DIR__) . '/../includes/header.php'; ?>
+	<main id="content" class="main">
+		<div class="full-width_hero"></div>
+		<article class="full-width_content animate__animated animate__fadeIn">
+			<div class="wrap">
+				<header class="content-header">
+					<h1 class="primary-heading">How to Start Freelancing as a Frontend Developer</h1>
+					<div class="content-meta">
+						<div class="meta-item by-author">
+							<figure class="meta-image">
+								<img src="<?php echo BASE_URL; ?>images/angelajholden-software-engineer-frontend-developer.jpg" alt="Angela J Holden | Software Engineer &amp; Frontend Developer">
+							</figure>
+							<span>By Angela</span>
+						</div>
+						<div class="meta-dots"></div>
+						<div class="meta-group">
+							<div class="meta-item">
+								<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M256 48C141.13 48 48 141.13 48 256s93.13 208 208 208 208-93.13 208-208S370.87 48 256 48zm96 240h-96a16 16 0 01-16-16V128a16 16 0 0132 0v128h80a16 16 0 010 32z"/></svg>
+								<time datetime="2025-06-15">Jun 15, 2025</time>
+							</div>
+							<div class="meta-item">
+								<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M400 480a16 16 0 01-10.63-4L256 357.41 122.63 476A16 16 0 0196 464V96a64.07 64.07 0 0164-64h192a64.07 64.07 0 0164 64v368a16 16 0 01-16 16z"/></svg>
+								<span>Tips &amp; Advice</span>
+							</div>
+						</div>
+					</div>
+				</header>
+				<div class="content-item">
+					<p>As soon as I started to learn HTML and CSS, I started freelancing. Thank god I was naive—because if I'd known what I was getting into, I probably wouldn't have tried at all.</p>
+					<p>It's the hardest thing I've ever done, and that includes actually writing code. From actually finding clients — to understanding what they need — to getting paid. It is either exhilarating or it will make you want to sit in the corner and cry.</p>
+					<p>This isn't an exhaustive guide. It's meant to be a starting point. I'll be expanding on these topics in future videos, so drop your questions in the comments if there's something you want me to dive deeper into.</p>
+
+					<h2>How to Find Clients</h2>
+					<p>I know the first thing you're wondering is: How do I find clients?</p>
+					<p>Well, I'm here to tell you that prospective clients are everywhere. Here's how I met my current client list:</p>
+
+					<p><strong>The dog park!</strong> Literally, I was just BS'ing with people at the dog park, and when someone asked me what I do for a living, I said freelance web designer, and that's how it started. It went from a conversation about how much a new website costs, to actually being hired to build a custom website. That was eight years ago and I still manage that site.</p>
+
+					<p>Another client heard me on a podcast about Etsy, because at one point I was selling graphic design on Etsy. But I was also a freelance web designer, and her business needed a new website. But here's the thing, she heard me on that podcast more than a year after it was originally published, and now she's been my client for nine years.</p>
+
+					<p>Another of my clients had a WordPress website that was so badly compromised attackers had login credentials and were using his blog to post vile ads using his domain credibility. At that time I was a WordPress developer and I was able to rebuild the site and restore his SEO ranking in search results.</p>
+
+					<p>And lastly, I can't even count the number of clients that I've gotten from running ads on Craigslist.</p>
+
+					<p>Yes, Craigslist. And listen — the bar is on the floor on Craigslist. Sometimes I go on Craigslist to look for something, and the things people post are so poorly written I wonder why they even bothered posting at all. So when I include screenshots of my work in the image gallery and write ad copy using complete sentences, those ads get noticed.</p>
+
+					<p>Now, when I started freelancing in 2014, sites like UpWork and Fiverr were just starting to get popular. I tried to win jobs on UpWork and was always outbid by developers who could work for much less than I could.</p>
+
+					<p>So if you can build a freelancing business that way, congratulations — because I thought it was really hard. What ended up working best for me was placing ads on my local Craigslist, and building relationships in my community. And pretty soon I didn't need to run ads at all because I had enough referrals and repeat business that I stayed really busy.</p>
+
+					<p>The point here is this: finding clients requires creativity and a willingness to talk to people, wherever you find yourself.</p>
+
+					<h2>Understanding Client Needs</h2>
+					<p>Once people show interest in your services, then what? Well, you need to figure out what problem you help them solve.</p>
+
+					<p>If they sell a service or product, do they need an e-commerce site, or do they need to drive traffic to a platform like eBay or Etsy? Every business owner has an elevator pitch. Listen to the pitch and try to understand what they're asking for. If they already have a website, listen to what they say about how the current site performs.</p>
+
+					<p>Some clients know exactly what they want. Some have no idea. Your job is to lead them through that process. But that all starts with understanding their goals and objectives.</p>
+
+					<h2>Writing Proposals</h2>
+					<p>Next, you need to write a proposal. When I wrote proposals, I restated what I understood their problem was, and how I thought I could solve that problem.</p>
+
+					<p>Keep in mind that a proposal is not the same as a scope of work. Think of the proposal as the pitch and the SOW as the contract. The proposal gets the client on board, and the SOW keeps the project on track.</p>
+
+					<p>In my early days as a freelancer I wrote detailed proposals that outlined all of the work I planned to do, and sure enough I never heard from those people again. I recall one in particular who insisted I include as many details as possible, and after the hours I spent doing research and writing that document, they turned around and handed it to their in-house developer who implemented all of my ideas. I got nothing for my time or trouble.</p>
+
+					<p><strong>Lesson learned:</strong> proposals aren't freebies. They're previews — not blueprints.</p>
+
+					<p>A proposal is a document that states you understand what the client needs, it estimates the amount of time it will take to complete the work, and it includes your price. When I was freelancing I had fixed pricing and I required a 50% deposit to hold a place on my calendar. Without that deposit, I wouldn't commit to any start date.</p>
+
+					<p>Once a new client accepts your proposal, the first thing I recommend is agreeing on a start date and sending the first invoice. Once the invoice is paid, the project goes on the calendar.</p>
+
+					<h2>Scope of Work (SOW)</h2>
+					<p>Okay, you have the deposit. Next, you should write a scope of work. This is a document that both parties will rely on for the entirety of the project. This is the detailed blueprint of work that you'll be doing. It's how you both know when something is done, and it's how you'll prevent scope creep.</p>
+
+					<p>By the way, scope creep happens on a lot of projects. Early on, you'll make mistakes and you'll have to absorb some of it, but as you gain experience, a detailed scope of work will help you manage it. Always refer back to the scope when clients request changes.</p>
+
+					<p>As you take on more projects, you'll get a good sense for how to control scope creep. It took me a long time to get a handle on it. But when I started writing really detailed scopes, that's how I got it under control. Because I could always refer back to that document as the final authority.</p>
+
+					<p>So, accepting that scope creep happens — and knowing how to deal with it immediately — you can actually turn it into ongoing business. And really, this is learning to manage expectations.</p>
+
+					<p>When a client asks for additional work that hasn't been agreed to, say something like: <strong>“Let's save that for phase two.”</strong></p>
+
+					<p>And if there's work that a client really wants done as part of the original job, be clear from the beginning about how it changes the timeline and increases the price. If the client doesn't agree to pay you for extra work, and allow the time to complete it, don't agree to it.</p>
+
+					<p>A scope of work is a document the client agrees to. And this goes back to you understanding what was being asked of you to begin with. It's okay if there's some back and forth when you're agreeing to the SOW, but by this point you should both be clear on the core expectations of the project.</p>
+
+					<h2>Timeline of Progress</h2>
+					<p>Next, I want you to create a timeline where you present work in phases to the client. Under no circumstance should you build an entire website and not show it to the client until it's finished. That's how you end up doing five rounds of revisions after launch.</p>
+
+					<p>Clients should agree to all work as it's being performed. And you should be meeting the deadlines you provided in the SOW.</p>
+
+					<p>In the beginning, set up in-person meetings or Zoom calls to present the work so you can answer questions and explain your progress. Sending off an email with a link to a staging site, or a PDF file with the design attached will not get the results you think it will. Clients need to be told what to look at, and you need to explain your progress and the process.</p>
+
+					<p>Imagine a client who's paying to have a website built for the first time, opening a link for a development site filled with Lorem Ipsum text. They won't know what they're looking at or how to respond appropriately if you aren't there presenting the work and telling them where their focus should be.</p>
+
+					<p>When you present work in person, you can explain what a staging site is — and while the site is in development — how it won't always look perfect as functionality is being implemented.</p>
+
+					<h2>Developer QA &amp; Client Review</h2>
+					<p>Ok, you've built the website and the client is happy and excited about the work. Next, you need a plan for QA and review.</p>
+
+					<p>Start by checking major browsers (Chrome, Firefox, Safari, Edge) and both Mac and Windows. Test on iOS and Android, too. Do not underestimate the number of issues you'll discover. Because you will find issues. Assume nothing works until you check it.</p>
+
+					<p>If you don't have access to enough devices to be thorough, either subscribe to a service like Browser Stack or buy some used devices on eBay. I personally prefer to keep a few devices handy, but do whatever works for you.</p>
+
+					<h2>Accessibility</h2>
+					<p>Next, make sure the site is accessible. If you're new to frontend development and accessibility, I suggest you start with the <a target="_blank" href="https://webaim.org/projects/million/">WebAIM Million</a> project. This project checks the top one million websites every year and documents the six most common accessibility mistakes.</p>
+
+					<p>And spoiler alert — it's the same six every year. If you can understand these six issues and make sure the sites you build never have them, you'll have a very good start when it comes to accessibility.</p>
+
+					<p>In general, when you perform QA, check the site from top to bottom. Is the margin and padding consistent? Are the fonts the correct size? Do you use anchor elements for links, and button elements to perform actions? Does each page have a meta title and description?</p>
+
+					<p>If the staging site is <code>noindex, nofollow</code>, make sure you remove that meta tag when it's deployed to production.</p>
+
+					<h2>Client Review</h2>
+					<p>How will your client review their new site before it's launched? Are you going to give them a checklist or just turn them loose and wait for feedback?</p>
+
+					<p>Based on how I phrased that question, which do you think it should be? Exactly.</p>
+
+					<p>Create a document or spreadsheet with specific items the client is responsible for reviewing. This is important, because without a checklist, you risk endless requests after launch. A clear checklist ensures both sides agree on what's done before going live.</p>
+
+					<h2>Launch &amp; Final Payment</h2>
+					<p>Ok, this next part can be tricky, but it doesn't have to be. Do you wait to launch the site until after you get the final payment, or can you launch, and then send the final invoice? You need to decide what works for you.</p>
+
+					<p>Based on my own personal experience, I launched the site first, and then I sent the final invoice. By the time you get to the launch phase of the project, you should have a very good sense for how engaged your client was in the process.</p>
+
+					<p>For me, this was an indicator of whether or not I felt confident about getting that final 50%. There was only one time I took a site down for non-payment, and the client sent me an email saying “Oh, I guess you want to get paid.”</p>
+
+					<p>And in hindsight it wasn't a surprise that he was dragging his feet, because he was a pain in the ass to work with, and his non-payment was another indication of his lack of respect. Don't be afraid to pull the plug. You're not a hostage.</p>
+
+					<h2>Conclusion</h2>
+					<p>I'll leave you with some final thoughts. The best advice I ever received was that it's okay to turn away a bad client, because then you'll have time for a good client.</p>
+
+					<p>By the way, it's also okay to take a bad client if you need the work. Sometimes when you need to pay the rent, you take on someone who you know will be difficult to work with. But being difficult doesn't equal someone who won't pay you. So raise your rate accordingly.</p>
+
+					<p>Around here, we call that the <strong>bullshit tax</strong>.</p>
+
+					<p>If you found this helpful, you can <a href="https://www.youtube.com/watch?v=KsjGVL6-MJk&t=3s" target="_blank">watch the original video on my YouTube channel</a> and leave a comment with any freelancing topics you'd like me to cover next. I've got plenty of stories and tips to share!</p>
+
+				</div>
+				<footer class="content-footer">
+					<?php include_once dirname(__DIR__) . '/../includes/author.php'; ?>
+					<?php include_once dirname(__DIR__) . '/../includes/comment-form.php'; ?>
+				</footer>
+			</div>
+		</article>
+	</main>
+	<?php include_once dirname(__DIR__) . '/../includes/footer.php'; ?>
+</body>
+
+</html>
